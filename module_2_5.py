f4fdeac5fb2583368728cd9fb0d790b822feadf7
natashaseroff <natashaseroff@mail.ru>
2024-10-05 14:34:06 +0200

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,8 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(m):
+            matrix[i].append(value)
+    return matrix
+print(get_matrix(4, 2, 6))
